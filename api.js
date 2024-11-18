3cc47c011657e373be09f9c3c47a327bbf585eba
sairamlaxmanreddy-burugapalli <sairamlaxmanreddy.burugapalli@slu.edu>
2024-11-18 06:19:21 +0000

    initial changes completed

---
+++ api.js
@@ -0,0 +1,76 @@
+
+const path = require('path')
+const Products = require('./products')
+const autoCatch = require('./lib/auto-catch')
+
+
+/**
+ * Handle the root route
+ * @param {object} req
+ * @param {object} res
+*/
+function handleRoot(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  }
+  
+  /**
+ * List all products
+ * @param {object} req
+ * @param {object} res
+ */
+async function listProducts (req, res) {
+    // Extract the limit and offset query parameters
+    const { offset = 0, limit = 25, tag } = req.query
+    // Pass the limit and offset to the Products service
+    res.json(await Products.list({
+      offset: Number(offset),
+      limit: Number(limit),
+      tag
+    }))
+  }
+
+  
+/**
+ * Get a single product
+ * @param {object} req
+ * @param {object} res
+ */
+async function getProduct (req, res, next) {
+    const { id } = req.params
+  
+    const product = await Products.get(id)
+    if (!product) {
+      return next()
+    }
+    
+    return res.json(product)
+  }
+
+ /**
+ * Create a new product
+ * @param {object} req
+ * @param {object} res
+ */
+async function createProduct (req, res) {
+    console.log('request body:', req.body)
+    res.json(req.body)
+  } 
+
+  async function deleteProduct (req, res) {
+    console.log('request body:', req.body)
+    res.json(req.body)
+  } 
+
+  
+  async function putProduct (req, res) {
+    console.log('request body:', req.body)
+    res.json(req.body)
+  } 
+  module.exports = autoCatch({
+    handleRoot,
+    listProducts,
+    getProduct,
+    createProduct,
+    deleteProduct,
+    putProduct
+  });
